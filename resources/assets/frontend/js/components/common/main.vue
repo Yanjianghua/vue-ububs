d5918d4854ebeb1338e24da31bc13ec793a942f9
linlm <linlm1994@gmail.com>
2018-01-14 10:55:16 +0800

    test github password

--- resources/assets/frontend/js/components/common/main.vue
+++ resources/assets/frontend/js/components/common/main.vue
@@ -8,30 +8,30 @@
                         <Menu mode="horizontal" :theme="theme1" active-name="1">
                             <MenuItem name="1">
                             <Icon type="ios-paper"></Icon>
-                            内容管理
+                            git文档
                             </MenuItem>
                             <MenuItem name="2">
                             <Icon type="ios-people"></Icon>
-                            用户管理
+                            swoole文档
                             </MenuItem>
                             <Submenu name="3">
                                 <template slot="title">
                                     <Icon type="stats-bars"></Icon>
-                                    统计分析
+                                    PHP相关
                                 </template>
-                                <MenuGroup title="使用">
+                                <MenuGroup title="基础">
                                     <MenuItem name="3-1">新增和启动</MenuItem>
                                     <MenuItem name="3-2">活跃分析</MenuItem>
                                     <MenuItem name="3-3">时段分析</MenuItem>
                                 </MenuGroup>
-                                <MenuGroup title="留存">
+                                <MenuGroup title="进阶">
                                     <MenuItem name="3-4">用户留存</MenuItem>
                                     <MenuItem name="3-5">流失用户</MenuItem>
                                 </MenuGroup>
                             </Submenu>
                             <MenuItem name="4">
                             <Icon type="settings"></Icon>
-                            综合设置
+                            linux服务器
                             </MenuItem>
                         </Menu>
                     </div>
